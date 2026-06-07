d34c7b69e9762c969f2a6d062ec933a333cdec3f
Max <emjotka05@gmail.com>
2026-06-08 00:06:20 +0200

    feat: add login and jwt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const { getUserByEmail, createUser } = require('../models/userModel');
 
 const register = async (req, res) => {
@@ -33,4 +35,51 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+    try {
+        const { email: rawEmail, password } = req.body;
+        const email = rawEmail.toLowerCase().trim();
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Email and password required."
+            });
+        }
+        const users = await getUserByEmail(email);
+        if (users.length === 0) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+        const user = users[0];
+        const isValid = await bcrypt.compare(password, user.password)
+        if (!isValid) {
+            return res.status(401).json({
+                message: "Invalid credentials."
+            });
+        }
+
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT,
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).json({
+            message: 'Login succesful.',
+            token: token
+        });
+
+    } catch (error) {
+        console.error("🔥 BŁĄD KRYTYCZNY LOGOWANIA:", error);
+        res.status(500).json({
+            message: "Something went wrong."
+        });
+
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
